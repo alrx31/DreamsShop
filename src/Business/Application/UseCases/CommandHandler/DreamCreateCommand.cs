47d27c36fb28f758be9c5b7c1ac8c7a0050ac09b
alrx31 <usov.lesha2017@gmail.com>
2025-05-30 23:09:34 +0300

    UPD

--- src/Business/Application/UseCases/CommandHandler/DreamCreateCommand.cs
+++
@@ -1,9 +0,0 @@
-using Application.DTO;
-using MediatR;
-
-namespace Application.UseCases.CommandHandler;
-
-public class DreamCreateCommand(DreamCreateDto dto) : IRequest
-{
-    public DreamCreateDto Dto { get; set; } = dto;
-}
--- src/Business/Infrastructure/Persistence/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Domain.Entity;
-using Microsoft.EntityFrameworkCore;
-
-namespace Infrastructure.Persistence;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
-{
-    public DbSet<Dream> Dream { get; init; }
-}
