f48b42dcb0f4a0db0526ff88472b1f8756eccd60
alrx31 <usov.lesha2017@gmail.com>
2025-06-07 16:39:45 +0300

    Fix Access Token

--- src/Idenitity/Presentation/DI/PresentationDependencies.cs
+++ src/Idenitity/Presentation/DI/PresentationDependencies.cs
@@ -57,13 +57,9 @@ public static class PresentationDependencies
                 var jwtSettings = configuration.GetSection("Jwt");
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
                     ValidateLifetime = true,
                     ValidateIssuerSigningKey = true,
 
-                    ValidIssuer = jwtSettings["Issuer"],
-                    ValidAudience = jwtSettings["Audience"],
                     IssuerSigningKey = new SymmetricSecurityKey(
                         Encoding.UTF8.GetBytes(jwtSettings["Key"]))
                 };
