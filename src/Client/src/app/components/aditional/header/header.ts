dc5789f76c1a21655c15b9e8d74ca7a81a6df98e
alrx31 <usov.lesha2017@gmail.com>
2025-07-31 21:13:57 +0300

    add client logout

--- src/Client/src/app/components/aditional/header/header.ts
+++ src/Client/src/app/components/aditional/header/header.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core';
-import {MatHeaderCell, MatHeaderRow} from '@angular/material/table';
-import {MatExpansionPanelHeader} from '@angular/material/expansion';
-import {MatTabNavPanel} from '@angular/material/tabs';
-import {MatToolbar} from '@angular/material/toolbar';
-import {MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
-import {MatNavList} from '@angular/material/list';
-import {MatIcon} from '@angular/material/icon';
-import {RouterLink, RouterOutlet} from '@angular/router';
 import {MatMenu, MatMenuTrigger} from '@angular/material/menu';
+import { Logout } from "../logout/logout";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [
-    RouterLink
-  ],
+    RouterLink,
+    Logout
+],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
